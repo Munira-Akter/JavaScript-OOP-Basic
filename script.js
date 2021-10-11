b3d8975db862c11231c2b2984a0cc82aea2e0ff4
Munira Akter <munira660602@gmail.com>
2021-10-11 16:08:36 +0600

    OOP Method & Prroperty

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Construction function
-function Student() {
-    this.name = "Munira";
+function Student(name, email, cell) {
+    this.name = name;
+    this.email = email;
+    this.cell = cell;
+
+    this.info = function() {
+        return `Cantact info of ${name} = Email : ${email}  Cell : ${cell}`;
+    };
 }
 
-let stu = new Student();
+let stu = new Student("Munira", "muniraweb@gmail.com", "01881959920");
 
-console.log(stu);
+console.log(stu.info());
