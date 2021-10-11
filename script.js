6fbfee94e74bb646f7fac6ee734f87826627d685
Munira Akter <munira660602@gmail.com>
2021-10-11 15:50:16 +0600

    JavaScript Basic OOP Concept Folder Ready

---
+++ script.js
@@ -0,0 +1,18 @@
+let student = {
+    name: "Munira",
+    age: 21,
+    job: "MERN",
+};
+
+console.log(student);
+
+function student() {
+
+    this.name = '',
+        this.name = '',
+        this.name = '',
+        this.name = '',
+
+}
+
+let stu = new student();
